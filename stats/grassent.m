d4733cfe8082a1e481c51e5a0b166e1b4be0cd4e
Luigi Acerbi <luigi.acerbi@nyu.edu>
2018-02-14 14:19:05 +0100

    added real() to prevent complex numbers from popping up for no reason

--- stats/grassent.m
+++ stats/grassent.m
@@ -1,4 +1,4 @@
-function H = grassent(x)
+function [H,G] = grassent(x)
 %GRASSENT Grassberger estimator of entropy for discrete distributions.
 %   H = grassent(X) returns the estimated entropy of a discrete array of 
 %   counts X. If X is a matrix, GRASSENT computes the entropy for each row.
@@ -29,8 +29,11 @@ h = unique([x(:); n(:)]);   % List of counts, including the G(N_k)
 
 % Grassberger numbers shifted by one to include G(0)
 G = NaN(1,1+max(h));
-G(h+1) = psi(h) + 0.5*(-1).^h .*(psi(0.5*(h+1)) - psi(0.5*h));
+G(h+1) = real(psi(h) + 0.5*(-1).^h .*(psi(0.5*(h+1)) - psi(0.5*h)));
 G(1) = 0;
+
+% Note: Added real() to prevent imaginary numbers from popping up in PSI
+% (this didn't happen before, something messed up with PSI?)
 
 % Entropy estimate
 base = zeros(size(n));
